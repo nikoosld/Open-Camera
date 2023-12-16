9a1e775e4d5d3995b5b829a024ad7d8e7dfc918e
Nikoo Salehfard <nikoosld@gmail.com>
2023-12-16 12:42:33 +0100

    Use Camera intent

--- app/src/main/java/com/example/open_camera/MainActivity.kt
+++ app/src/main/java/com/example/open_camera/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.open_camera
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 
+val REQUEST_IMAGE_CAPTURE = 123
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btnOpenCamera)
+        button.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
