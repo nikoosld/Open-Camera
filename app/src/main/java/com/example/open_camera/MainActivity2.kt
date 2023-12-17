a49f19b742338ce584b698b887bc0c76328ca8d4
Nikoo Salehfard <nikoosld@gmail.com>
2023-12-17 18:18:07 +0100

    final

--- app/src/main/java/com/example/open_camera/MainActivity2.kt
+++ app/src/main/java/com/example/open_camera/MainActivity2.kt
@@ -3,12 +3,6 @@ package com.example.open_camera
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.Directory
-import android.widget.Button
-import androidx.camera.camera2.internal.annotation.CameraExecutor
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
