56aa9b37b6980e9edf5faa7a746b617c539c674b
Nikoo Salehfard <nikoosld@gmail.com>
2023-12-17 06:00:36 +0100

    Turn flashlight on

--- app/src/main/java/com/example/open_camera/MainActivity.kt
+++ app/src/main/java/com/example/open_camera/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.open_camera
 
+import android.content.Context
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    private var cameraManager : CameraManager? = null
+    private var getCameraID : String? = null
+    private var torchStatus : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.btnOpenCamera)
+        cameraManager = applicationContext.getSystemService(Context.CAMERA_SERVICE)as CameraManager
+        try {
+            getCameraID = cameraManager!!.cameraIdList[0]
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        button.setOnClickListener {
+            try {
+                cameraManager!!.setTorchMode(getCameraID!!,true)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
     }
 }
